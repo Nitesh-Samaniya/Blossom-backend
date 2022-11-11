90bc5b7906a775c078c5b031299d77e54548340d
Nitesh-Samaiya <nitinsamaniya97@gmail.com>
2022-11-11 13:43:02 +0530

    backend for blossom

---
+++ src/config/db.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const Connect = ()=>{
+    return mongoose.connect(`mongodb+srv://nitesh:samaniya@cluster0.fq7r0cr.mongodb.net/blossom`);
+}
+
+module.exports = Connect;
