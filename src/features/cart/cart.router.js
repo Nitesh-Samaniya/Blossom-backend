588f650a91a722f38414db122b2a90c4a1502160
Nitesh-Samaiya <nitinsamaniya97@gmail.com>
2022-12-24 16:00:32 +0530

    alter cart item

--- src/features/cart/cart.router.js
+++ src/features/cart/cart.router.js
@@ -30,6 +30,29 @@ app.post("/:id", async(req,res)=> {//user ki id -> add product to cart
     }
 })
 
+app.patch("/:id", async(req,res)=> { // document ki object _id and body me increased qyt
+    const {id} = req.params;
+    // console.log(id)
+    try{
+        const afterUpdate = await Cart.findByIdAndUpdate(id, {$set: {quantity: req.body.quantity}}, {new: true});
+        return res.status(200).send(afterUpdate);
+    }catch(e){
+        return res.status(401).send(e);
+    }
+})
+
+
+app.delete("/all/:id", async(req,res)=> {//user ki id se
+    const {id} = req.params;
+    // console.log(id)
+    try{
+        const deleteAllitems = await Cart.deleteMany({userId: id});
+        return res.status(201).send({ message: "deleted all data", deleteAllitems});
+    }catch(e){
+        return res.status(400).send(e)
+    }
+})
+
 app.delete("/:id", async(req,res)=> {
     try{
         const id = req.params.id;
